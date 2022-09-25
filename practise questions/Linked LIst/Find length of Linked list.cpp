a5741030ae42da862b4180d7e99a056626bdbd22
himsapphire <87421281+himsapphire@users.noreply.github.com>
2022-09-25 09:10:00 +0530

    Update Find length of Linked list.cpp

--- practise questions/Linked LIst/Find length of Linked list.cpp
+++ practise questions/Linked LIst/Find length of Linked list.cpp
@@ -1,26 +1,27 @@
 /* this ques also handles the case when there is self loop of 1 node only*/
 
 
-if(!head or !head->next)
-            return 0;
-        Node* slow=head;
-        Node* fast= head;
-        while(fast and fast->next){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast==slow)
-              break;
+int countNodesinLoop(struct Node *head)
+{
+    Node *fast, *slow;
+    fast = slow = head;
+    
+    Node* loop = NULL;
+    
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast == slow){
+            loop = fast;
+            break;
         }
-        slow=head;
-        while(slow!=fast){
-            slow=slow->next;
-            fast=fast->next;
-        }
-        int c=1;
-        slow=slow->next;
-        while(fast!=slow){
-            slow=slow->next;
-            c++;
-        }
-        return c;
+    }
+    int c = 0;
+    if(!loop) return 0;
+    Node *looper = loop;
+    while(loop->next != looper){
+        c++;
+        loop=loop->next;
+    }
+    return c+1;
 }
